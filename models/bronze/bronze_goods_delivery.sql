c49f2aecf6518594a63df66c56cf7999647a7873
jacobdbt2100 <jacobjoshua675@gmail.com>
2026-01-31 17:33:01 +0100

    moved model to bronze

--- models/bronze/bronze_goods_delivery.sql
+++ models/bronze/bronze_goods_delivery.sql
@@ -1,17 +1,17 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-select
-    "Order ID" as order_id,
-    "Article" as article,
-    "Source Location" as warehouse,
-    "Target Location" as customer,
-    "Via" as transport_mode,
-    "Shipping Date" as shipped_date,
-    "Planned Delivery Date" as planned_delivery_date,
-    "Actual Delivery Date" as actual_delivery_date,
-    "Planned Qty" as planned_qty,
-    "Weight (Ton)" as weight_ton,
-    "Actual Delivered" as actual_qty_delivered,
-    "Status" as status,
-    "Cost" as cost
+select 
+  `Order ID` as order_id,
+  `Article` as article,
+  `Source Location` as warehouse,
+  `Target Location` as customer,
+  `Via` as transport_mode,
+  `Shipping Date` as shipped_date,
+  `Planned Delivery Date` as planned_delivery_date,
+  `Actual Delivery Date` as actual_delivery_date,
+  `Planned Qty` as planned_qty,
+  `Weight (Ton)` as weight_ton,
+  `Actual Delivered` as actual_qty_delivered,
+  `Status` as status,
+  `Cost` as cost
 from {{ source('goods_delivery_data', 'goods_delivery') }}
